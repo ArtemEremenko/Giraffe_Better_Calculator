2e3e429dec538d39cb4728dabfba7a16cd4117f6
EremenkoArtem <Eremenko_Artem@i.ua>
2023-08-03 14:20:02 +0300

    Removed all code repeats

--- Giraffe_Better_Calculator/Program.cs
+++ Giraffe_Better_Calculator/Program.cs
@@ -1,26 +1,63 @@
 ﻿using System.Diagnostics.Metrics;
+using System.Dynamic;
 using System.Globalization;
 
 internal class Program
 {
     private static void Main(string[] args)
 
+
     {
+
         CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 
-        Console.WriteLine("Enter number 1 ");
-        float num1;
-        while (!float.TryParse(Console.ReadLine(), out num1))
+        Console.WriteLine("Enter number 1");
+        float num1 = GetFloatInput();
+
+        Console.WriteLine("Enter operator");
+        string oper = GetOperInput();
+
+        Console.WriteLine("Enter number 2");
+        float num2 = GetFloatInput();
+
+        string answ = "Answer is ";
+
+        if (oper == "+")
+        {
+            Console.WriteLine(answ + (num1 + num2));
+        }
+        else if (oper == "-")
+        {
+            Console.WriteLine(answ + (num1 - num2));
+        }
+        else if (oper == "*")
+        {
+            Console.WriteLine(answ + (num1 * num2));
+        }
+        else if (oper == "/")
+        {
+            Console.WriteLine(answ + (num1 / num2));
+        }
+
+    }
+    private static float GetFloatInput()
+    {
+        float num;
+        while (!float.TryParse(Console.ReadLine(), out num))
         {
             Console.WriteLine("Enter float please");
         }
-         
-        Console.WriteLine("Enter operator");
-        string operato;
-        while (true) 
+        
+        return num;
+    }
+
+    private static string GetOperInput()
+    {
+        string oper;
+        while (true)
         {
-            operato = Console.ReadLine();
-            if (operato == "+" || operato == "-" || operato == "*" || operato == "/")
+            oper = Console.ReadLine();
+            if (oper == "+" || oper == "-" || oper == "*" || oper == "/")
             {
                 break;
             }
@@ -29,33 +66,58 @@ internal class Program
                 Console.WriteLine("Enter operator + - * /");
             }
         }
+        return oper;
 
-        Console.WriteLine("Enter number 2 ");
-        float num2;
-        while (!float.TryParse(Console.ReadLine(), out num2))
-        {
-            Console.WriteLine("Enter float please");
-        }
 
-        if (operato == "+")
-        {
-            Console.WriteLine("Answer is " + (num1 + num2));
-        }
-        else if (operato == "-")
-        {
-            Console.WriteLine("Answer is " + (num1 - num2));
-        }
-        else if (operato == "*")
-        {
-            Console.WriteLine("Answer is " + (num1 * num2));
-        }
-        else if (operato == "/")
-        {
-            Console.WriteLine("Answer is " + (num1 / num2));
-        }
-        else
-        {
-            Console.WriteLine("Wrong operator");
-        }
+        //Console.WriteLine("Enter number 1 ");
+        //float num1;
+
+        //while (!float.TryParse(Console.ReadLine(), out num1))
+        //{
+        //    Console.WriteLine("Enter float please");
+        //}
+
+        //Console.WriteLine("Enter operator");
+        //string operato;
+        //while (true) 
+        //{
+        //    operato = Console.ReadLine();
+        //    if (operato == "+" || operato == "-" || operato == "*" || operato == "/")
+        //    {
+        //        break;
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Enter operator + - * /");
+        //    }
+        //}
+
+        //Console.WriteLine("Enter number 2 ");
+        //float num2;
+        //while (!float.TryParse(Console.ReadLine(), out num2))
+        //{
+        //    Console.WriteLine("Enter float please");
+        //}
+
+        //string answ = ("Answer is ");
+
+        //if (operato == "+")
+        //{
+        //    Console.WriteLine(answ + (num1 + num2));
+        //}
+        //else if (operato == "-")
+        //{
+        //    Console.WriteLine(answ + (num1 - num2));
+        //}
+        //else if (operato == "*")
+        //{
+        //    Console.WriteLine(answ + (num1 * num2));
+        //}
+        //else if (operato == "/")
+        //{
+        //    Console.WriteLine(answ + (num1 / num2));
+        //}
     }
+    
+    
 } 
